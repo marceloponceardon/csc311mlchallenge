4db5204dca8e7a197a818f2a0f61dfdc1ce40e0a
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2024-03-04 18:19:36 -0500

    Linting check test

--- model/pred.py
+++ model/pred.py
@@ -8,21 +8,17 @@ and produces predictions for that test set.
 '''
 
 # The script can use the following libraries
-import numpy as np
-import pandas as pd
-
-# As well as basic python imports like the below
-import sys, csv, random
-
+# As well as basic python imports like sys, csv, random
 # You will NOT be able to use sklearn, pytorch, tensorflow, etc. in the final submitted code.
 # You may reuse any code that you wrote or was provided to you from the labs.
+# We should be able to make ~60 predictions within 1 minute (1 prediction per second)
 
-# Should be able to make ~60 predictions within 1 minute (1 prediction per second)
-
-'''
-TODO:
-
-'''
+from importlib.metadata import version
+import sys
+# import csv
+# import random
+# import numpy as np
+import pandas as pd
 
 def main():
     '''
@@ -31,6 +27,9 @@ def main():
     calls relevant functions to make predictions, 
     and writes to a csv file
     '''
+    # Print version of python, and import libraries using importlib
+    print("Pandas version: ", version('pandas'))
+    print("Numpy version: ", version('numpy'))
 
     # Check if the correct number of arguments are passed
     if len(sys.argv) != 2:
